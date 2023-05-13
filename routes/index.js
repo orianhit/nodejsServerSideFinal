e98f14c00c6e46a84ee8e1b39cc0e1e4ae0c605d
Orian <orianhit@hit.ac.il>
2023-05-13 17:47:25 +0300

    add comments

--- routes/index.js
+++ routes/index.js
@@ -9,27 +9,42 @@ const {Reports} = require('../model/reports.js');
 const {Users} = require('../model/users.js');
 const {currentMonth, currentYear} = require('../utils/date');
 
-/* GET home page. */
+// Define a route that responds to GET requests to the / path.
 router.get('/', function (req, res, next) {
     try {
+        // Send the title of the website to the client.
         res.status(200).json({title: 'Orian And Lital - HIT'});
     } catch (err) {
+        // Log the error.
+        console.error(err);
+
+        // Pass the error to the next middleware.
         next(err);
     }
 });
 
+// Define a route that responds to GET requests to the /clear path.
 router.get('/clear', async function (req, res, next) {
+    // Try to delete all reports and costs from the database.
     try {
         await Reports.deleteMany({});
         await Costs.deleteMany({});
+
+        // Send a success message to the client.
         res.status(200).json({message: 'success'});
     } catch (err) {
+        // Log the error.
+        console.error(err);
+
+        // Pass the error to the next middleware.
         next(err);
     }
 })
 
+// Define a route that responds to POST requests to the /addcost path.
 router.post('/addcost', async function (req, res, next) {
     try {
+        // Get the user ID, description, category, sum, year, month, and day from the request body.
         const user_id = req.body.user_id;
         const description = req.body.description;
         const category = req.body.category;
@@ -38,6 +53,7 @@ router.post('/addcost', async function (req, res, next) {
         const month = req.body.month;
         const day = req.body.day;
 
+        // Validate that the year, month, day, user ID, description, category, and sum are not empty.
         [['year', year], ['month', month], ['day', day], ['category', category], ['sum', sum],
             ['user_id', user_id], ['description', description]].forEach(function (field) {
             if (isEmpty(field[1])) {
@@ -45,11 +61,13 @@ router.post('/addcost', async function (req, res, next) {
             }
         })
 
+        // Check if the user exists in the database.
         const isUserIdExists = await Users.find({id: user_id});
         if (isUserIdExists.length === 0) {
             throw new InputValidationError(`user id ${user_id} does not exists`);
         }
 
+        // Create a new cost document in the database.
         const cost = await Costs.create({
             user_id: user_id,
             description: description,
@@ -60,34 +78,48 @@ router.post('/addcost', async function (req, res, next) {
             day: day,
         });
 
-        res.json(cost);
+        // Send the cost document to the client.
+        res.status(201).json(cost);
     } catch (err) {
+        // Log the error.
+        console.error(err);
+
+        // Pass the error to the next middleware.
         next(err);
     }
 })
 
+// Define a route that responds to GET requests to the /report path.
 router.get('/report', async function (req, res, next) {
     try {
+        // Get the year, month, and user ID from the request query parameters.
         const year = req.query.year;
         const month = req.query.month;
         const user_id = req.query.user_id;
 
+        // Validate that the year, month, and user ID are not empty.
         [['year', year], ['month', month], ['user_id', user_id]].forEach(function (field) {
             if (isEmpty(field[1])) {
                 throw new InputValidationError(`missing field ${field[0]}`);
             }
         })
 
+        // Check if the user exists in the database.
         const isUserIdExists = await Users.find({id: user_id});
         if (isUserIdExists.length === 0) {
             throw new InputValidationError(`user id ${user_id} does not exists`);
         }
 
+        // Get the cached report for the given year, month, and user ID.
+        let cachedCategory = await Reports.findOne({year: Number(year), month: Number(month), user_id: user_id});
 
-        let cachedCategory = await Reports.find({year: Number(year), month: Number(month), user_id: user_id});
+        // If there is no cached report, create a new one.
+        if (!cachedCategory) {
 
-        if (cachedCategory.length === 0) {
+            // Get the costs for the given year, month, and user ID.
             const costs = await Costs.find({year: Number(year), month: Number(month), user_id: Number(user_id)});
+
+            // Create a new report from the costs.
             cachedCategory = costs.reduce((groups, cost) => {
                 (groups[cost.category] = groups[cost.category] || []).push({
                     day: cost.day,
@@ -97,6 +129,7 @@ router.get('/report', async function (req, res, next) {
                 return groups;
             }, {});
 
+            // If the report is not of the current year and month, create it in the database.
             if (Number(year) !== currentYear() || Number(month) !== currentMonth()) {
                 await Reports.create({
                     ...cachedCategory,
@@ -105,10 +138,10 @@ router.get('/report', async function (req, res, next) {
                     user_id: user_id,
                 });
             }
-        } else {
-            cachedCategory = cachedCategory[0];
         }
 
+        // Send the report to the client.
+        // Setting empty array if category not in document AKA no cost exists
         res.json({
             food: cachedCategory.food || [],
             health: cachedCategory.health || [],
@@ -119,17 +152,40 @@ router.get('/report', async function (req, res, next) {
             other: cachedCategory.other || [],
         });
     } catch (err) {
+        // Log the error.
+        console.error(err);
+
+        // Pass the error to the next middleware.
         next(err);
     }
 })
 
+// Define a route that responds to GET requests to the /about path.
 router.get('/about', (req, res, next) => {
+    // Try to send the creators information.
     try {
-        res.json([
-            {firstname: 'Orian', lastname: 'Dabod', id: 308337062, email: 'oriandabud@gmail.com'},
-            {firstname: 'Lital', lastname: 'Kraft', id: 314806647, email: 'Litalkraft19@gmail.com'},
-        ]);
+        // set the creators info in variable
+        const creators = [
+            {
+                firstname: 'Orian',
+                lastname: 'Dabod',
+                id: 308337062,
+                email: 'oriandabud@gmail.com',
+            },
+            {
+                firstname: 'Lital',
+                lastname: 'Kraft',
+                id: 314806647,
+                email: 'Litalkraft19@gmail.com',
+            },
+        ];
+        // Send the user information to the client.
+        res.json(users);
     } catch (err) {
+        // Log the error.
+        console.error(err);
+
+        // Pass the error to the next middleware.
         next(err);
     }
 })
